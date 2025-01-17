d6a7a3fcaa725e39696ffa2ceed3db5e1d051303
nasser <nassir7abusaroor@gmail.com>
2025-01-17 23:45:01 +0300

    new commit

--- templates/index.php
+++ templates/index.php
@@ -4,9 +4,9 @@
     <title>Home</title>
 </head>
 <body>
-    <h1>Home</h1>
+    <h1>Home page</h1>
     <p>Welcome to the home page.</p>
-    <p><a href="about">About</a></p>
+    <p><a href="about">About me</a></p>
     <p><a href="hello/nasser">hello</a></p>
 </body>
 </html>
