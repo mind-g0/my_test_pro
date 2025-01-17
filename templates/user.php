77e9aa961de5ff79a4157b6c3ca0d48f313aced3
nasser <nassir7abusaroor@gmail.com>
2025-01-17 23:51:40 +0300

    new commit 3

--- templates/user.php
+++ templates/user.php
@@ -1,7 +1,21 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title>User</title>
+    <style>
+        p{
+            color: red;
+            font-size: 50px;
+        }
+    </style>
+</head>
 <h1> hello {{name}} </h1>
 <p> the replas shuld be in tow {}</p>
+
+<p> if i want to comit my update to github :</p>
+<p> i should use git add . </p>
+<p> then git commit -m "add my message" </p>
+<p> then < git push > </p>
 
 <h1> heer is an array</h1>
 
